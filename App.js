70a89ca5ed235a01ae8743c55a17d56e17e3f118
Harshini GU <149071617+Harshiniii027@users.noreply.github.com>
2025-02-17 09:52:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import InputComponent from './InputComponent';
+import Main from './Main';
+function App() {
+  return (
+    <Main/>
+  );
+}
+
+export default App;
